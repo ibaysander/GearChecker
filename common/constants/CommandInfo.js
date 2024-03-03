1318f495af621478a0d47ebc141a532b6c5c6837
Kiril Gaydarov <kikchan@users.noreply.github.com>
2024-03-03 19:59:11 +0100

    fix: Updated help command and README.md

--- common/constants/CommandInfo.js
+++ common/constants/CommandInfo.js
@@ -24,7 +24,7 @@ const Help =
             **!gems [player_name] [realm?]**: Displays which gems are missing from the player's currently equipped items.
             **!armory [player_name] [realm?]**: Returns a link to the player's armory.
             **!summary [player_name] [realm?]**: Lists all the details regarding the given player.
-            **!achievements [player_name] [realm?]**: Lists all the details regarding the given player.
+            **!achievements [player_name] [realm?]**: Displays a table with the achievement progress of the player.
             
             **[realm?]** is an optional parameter. By default = Icecrown.
             
@@ -34,7 +34,7 @@ const Help =
             !gs Metalforce
             !summary Koch Lordaeron
             !gs Koch Lordaeron
-            !
+            !achievements Metalforce
             
 **Feel free to join the official Discord server of the bot [here](https://discord.gg/ZSDpeftAB7) and ask/suggest me anything!**
 `;
