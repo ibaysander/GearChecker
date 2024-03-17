b2f10d2cb93b92b7ce4c6f6c278e20c764f48a1c
Kiril Gaydarov <kikchan@users.noreply.github.com>
2024-03-17 20:21:50 +0100

    fix: Fixed typo

--- GearChecker.bat
+++ GearChecker.bat
@@ -4,4 +4,4 @@ cd "C:\Program Files (x86)\Metalforce\GearChecker"
 echo "------------------------------------------------------------------------------------------------" >> start_log.txt
 echo Started: %DATE% %TIME% >> start_log.txt
 
-node start >> start_log.txt
+npm start >> start_log.txt
